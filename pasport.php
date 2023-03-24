b0a4a5e83060c09ba05a8bbddc81a309f4fb06ba
pfirulin <pfirulin@yandex.ru>
2023-03-24 09:58:27 +0800

    created index, pasport and program list

---
+++ pasport.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Наименование программы</title>
+</head>
+<body>
+    <h2 class="program__name">01.02.03 «Эффективное природопользование»</h2>
+
+    <section class="program__info">
+<!--        <artikle class="programm__-head list__item">-->
+<!--            <div>Дисциплина</div>-->
+<!--            <div>Знание</div>-->
+<!--            <div>Умение</div>-->
+<!--            <div>Навыки</div>-->
+<!--            <div>Преподователь</div>-->
+<!--            <div>Функции</div>-->
+<!--        </artikle>-->
+<!--        <artikle class="program__info-item list__item">-->
+<!--            <div> «Природоведение» </div>-->
+<!--            <div>-->
+<!--                Знает основные виды трав,-->
+<!--                растений, животных региона-->
+<!--            </div>-->
+<!--            <div>-->
+<!--                Умеет определять виды на-->
+<!--                основе признаков-->
+<!--            </div>-->
+<!--            <div>-->
+<!--                Владеет методами-->
+<!--                классификации на основе-->
+<!--                таксономических справочников-->
+<!--            </div>-->
+<!--            <div></div>-->
+<!--            <div></div>-->
+<!--        </artikle>-->
+        <table>
+           <thead>
+                <tr>
+                    <td>Дисциплина</td>
+                    <td>Знания</td>
+                    <td>Умения</td>
+                    <td>Навыки</td>
+                    <td>Преподователь</td>
+                    <td>Функции</td>
+                </tr>
+           </thead>
+            <tr>
+                <td>Природоведение</td>
+                <td>Знает основные виды трав, растений, животных региона</td>
+                <td>Умеет определять виды на основе признаков</td>
+                <td>Владеет методами классификации на основе таксономических справочников</td>
+                <td>Иван Иванович</td>
+            </tr>
+        </table>
+    </section>
+</body>
+</html>
---
+++ programs.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css">
+    <title>program list</title>
+</head>
+<body>
+    <sections class="program__list">
+        <artikle class="program__list-head list__item">
+            <div class="">Программа</div>
+            <div class="">Руководитель</div>
+        </artikle>
+        <artikle class="program__item list__item">
+            <div class="program__item-name">
+                <a href="">01.02.03 «Эффективное природопользование»</a>
+            </div>
+            <div class="program__item-head">
+                Селиванов семен петрович
+            </div>
+
+        </artikle>
+    </sections>
+</body>
+</html>
+
